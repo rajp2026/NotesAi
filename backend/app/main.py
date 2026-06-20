5298fc20a190ea37e44610601f4128f11ce8eeb5
rajp2026 <rajprakshit2026@gmail.com>
2026-06-20 18:54:03 +0530

    fix: update CORS origin to allow all domains in production

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,11 +21,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
 
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=["*"],
 
-    allow_credentials=True,
+    allow_credentials=False,
 
     allow_methods=["*"],
 
